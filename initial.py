f6bc12204bb148cfe60b435d7bc03aae07e0a9ab
devaaja <arto_chatlikov@hotmail.com>
2016-07-20 15:17:43 +0300

    Added basic vCenter connectivity

--- initial.py
+++ initial.py
@@ -1,24 +1,66 @@
 """Just a skellington to help me get started"""
 
 import atexit
+import argparse
+import getpass
 
 from pyVim import connect
 from pyVmomi import vmodl
-from pyVmomi import vim
 
-args = cli.get_args()
+## Couldn't figure out how to connect to vCenter in my case so I am using "hello_world_vcenter.py" as baseline
 
-try:
-    service_instance = connect.SmartConnect(host=args.host,
-                                            user=args.user,
-                                            pwd=args.password,
-                                            port=int(args.port))
+def get_args(): ## Using this as a temporary solution while thinking about how I can hide variables in the best possible way
+    """Get command line args from the user.
+    """
+    parser = argparse.ArgumentParser(
+        description='Standard Arguments for talking to vCenter')
 
-    atexit.register(connect.Disconnect, service_instance)
+    # because -h is reserved for 'help' we use -s for service
+    parser.add_argument('-s', '--host',
+                        required=True,
+                        action='store',
+                        help='vSphere service to connect to')
 
+    # because we want -p for password, we use -o for port
+    parser.add_argument('-o', '--port',
+                        type=int,
+                        default=443,
+                        action='store',
+                        help='Port to connect on')
+
+    parser.add_argument('-u', '--user',
+                        required=True,
+                        action='store',
+                        help='User name to use when connecting to host')
+
+    parser.add_argument('-p', '--password',
+                        required=False,
+                        action='store',
+                        help='Password to use when connecting to host')
+
+    args = parser.parse_args()
+
+    if not args.password:
+        args.password = getpass.getpass(
+            prompt='Enter password for host %s and user %s: ' %
+                   (args.host, args.user))
+    return args
 
 def main():
-	pass
+
+    args = get_args()
+    try:
+        service_instance = connect.SmartConnect(host=args.host,
+                                                user=args.user,
+                                                pwd=args.password,
+                                                port=int(args.port))
+
+        atexit.register(connect.Disconnect, service_instance)
+
+# Start program
+if __name__ == "__main__":
+    main()
+
 
 def vmrequest(userid, diskrequest,processorrequest,ramrequest): ## Incoming request from api with user defined parameters
 	pass
