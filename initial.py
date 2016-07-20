aeaa00fecfb806e173aa004fafe27c72ed55f4f6
devaaja <arto_chatlikov@hotmail.com>
2016-07-20 08:45:55 +0300

    A new hopee

--- initial.py
+++ initial.py
@@ -1,19 +1,36 @@
 """Just a skellington to help me get started"""
 
+import atexit
+
+from pyVim import connect
+from pyVmomi import vmodl
+from pyVmomi import vim
+
+args = cli.get_args()
+
+try:
+    service_instance = connect.SmartConnect(host=args.host,
+                                            user=args.user,
+                                            pwd=args.password,
+                                            port=int(args.port))
+
+    atexit.register(connect.Disconnect, service_instance)
+
+
 def main():
 	pass
 
-def requestedvm(diskrequest,processorrequest,ramrequest):
+def vmrequest(userid, diskrequest,processorrequest,ramrequest): ## Incoming request from api with user defined parameters
 	pass
 
-def createvm():
+def vmcreate(): ## Creating a virtual machine in vCenter
 	pass
 
-def startvm():
+def vmstart(): ## Starting a virtual machine in vCenter
 	pass
 
-def shutdownvm():
+def vmshutdown(): ## Shutting down a virtual machine in vCenter
 	pass
 
-def deletevm():
+def vmdelete(): ## Deleting a virtual machine from disk in vCenter
 	pass
